01a7c72e5787466a326c35ae320dc9d4a7415410
Kirsten Kwong <kskwong@ualberta.ca>
2019-02-07 18:06:50 -0700

    config changes, added theme files

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,6 @@ PATH = 'content'
 TIMEZONE = 'America/Edmonton'
 
 DEFAULT_LANG = 'en'
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -33,3 +32,5 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = 'templates/mytheme'
